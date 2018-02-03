591944cb42d93f08f3cbb2a40656a7987a91e599
ZhylkoIra <irazhylko@gmail.com>
2018-02-03 23:12:01 +0200

    new

---
+++ DFDS/Data/DFDSContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace DFDS.Models
+{
+    public class DFDSContext : DbContext
+    {
+        public DFDSContext (DbContextOptions<DFDSContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<DFDS.Models.Feedback> Feedback { get; set; }
+    }
+}
---
+++ DFDS/Models/Feedback.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DFDS.Models
+{
+    public class Feedback
+    {
+        public int ID { get; set; }
+        public string UserName { get; set; }
+        public string Description { get; set; }
+    }
+}
