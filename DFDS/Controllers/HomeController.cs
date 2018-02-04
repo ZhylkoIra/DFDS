bbd36e125a75a5d666423c9d53408eb26a081ee3
ZhylkoIra <irazhylko@gmail.com>
2018-02-04 16:46:44 +0200

    new

--- DFDS/Controllers/HomeController.cs
+++ DFDS/Controllers/HomeController.cs
@@ -15,16 +15,39 @@ namespace DFDS.Controllers
             return View();
         }
 
-        public IActionResult About()
-        {
-            ViewData["Message"] = "Your application description page.";
-
-            return View();
-        }
-
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        private readonly DFDSContext _context;
+
+        public HomeController(DFDSContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("ID,UserName,Description")] Feedback feedback)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(feedback);
+                await _context.SaveChangesAsync();
+                return RedirectPermanent("/Home/Index");
+            }
+            return View();
+        }
+
+        private bool FeedbackExists(int id)
+        {
+            return _context.Feedback.Any(e => e.ID == id);
         }
     }
 }
